cec08867ed7ccf0c9e67135b50185481c586afee
rohtanni <118976368+rohtanni@users.noreply.github.com>
2023-02-06 16:25:48 +0200

    Create f1.ts

---
+++ 01_funktsioon/f1.ts
@@ -0,0 +1,10 @@
+function korrutusKahega(arv: number): number{
+	return arv*2;
+}
+console.log(korrutusKahega(7));
+
+//funktsioon kahe arvu keskmise leidmiseks
+function keskmine (arv1: number, arv2: number): number{
+	return (arv1+arv2)/2;
+}
+console.log(keskmine(3,4));
